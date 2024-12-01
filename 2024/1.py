7743210a7536ee800fec803ac1d0de4d50d6aca1
Shiv Sitaram <181917858+shivsitaram@users.noreply.github.com>
2024-12-01 09:38:54 -0800

    created .gitignore and 24-1

---
+++ 2024/1.py
@@ -0,0 +1,21 @@
+with open("input") as f:
+    ar = [i.split() for i in f.readlines()]
+
+a = []
+b = []
+for i in ar:
+    a.append(int(i[0]))
+    b.append(int(i[1]))
+
+a.sort()
+b.sort()
+
+ans1 = 0
+ans2 = 0
+for i in range(len(a)):
+    ans1 += abs(b[i] - a[i])
+    ans2 += a[i] * b.count(a[i])
+
+print(ans1)
+print(ans2)
+
