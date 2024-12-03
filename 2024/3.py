221504808a3a675836cbd1fe0dd877708fcb576f
Shiv Sitaram <181917858+shivsitaram@users.noreply.github.com>
2024-12-02 21:29:55 -0800

    2024-3

---
+++ 2024/3.py
@@ -0,0 +1,29 @@
+with open("input", "r") as f:
+    s = f.read().strip()
+
+ans1 = 0
+ans2 = 0
+good = True
+
+while len(s) >= 4:
+    if s[:4] == "mul(":
+        ne = s.find(")")
+        if ne != -1:
+            nums = s[4:ne].split(",")
+            if len(nums) == 2 and all(i.isnumeric() for i in nums):
+                nums = list(map(int, nums))
+                ans1 += nums[0] * nums[1]
+                if good:
+                    ans2 += nums[0] * nums[1]
+
+    if s[:4] == "do()":
+        good = True
+
+    if len(s) >= 7 and s[:7] == "don't()":
+        good = False
+    
+    s = s[1:]
+
+print(ans1)
+print(ans2)
+
